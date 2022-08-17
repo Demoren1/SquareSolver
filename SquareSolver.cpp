7d3ad6b8d37dd027c27f25d583ffc96cfd854ebd
Demoren1 <72669171+Demoren1@users.noreply.github.com>
2022-08-18 01:37:08 +0300

    Пофиксил кодстайл, коэфы теперь имеют тип float

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,44 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 
-int test_int(void);      //check number on integrity, if int - return
+double test_num(void);      //check number on integrity, if int - return
 int main(void)
 {
     char exit = 'A';
-    double x1=0, x2=0;
-    int a=0, b=0, c=0, D=0, flag=0;     //if flag == 1 - lit in buffer
+    double x1 = 0, x2 = 0, a = 0, b = 0, c = 0, D = 0;
 
     printf("Input coefficients for equation a*x^2+b*x+c=0 \n");
 
     printf("a=");
-    a = test_int();
+    a = test_num();
     printf("b=");
-    b = test_int();
+    b = test_num();
     printf("c=");
-    c = test_int();
+    c = test_num();
 
-    while (getchar() != EOF){
-            if (a ==0 && b ==0)
-                printf("%s", (c==0) ? "x belong R": "x belong empty set");
-            else if (a==0){                                                         //IF equation isn't square
-                    x1=x2=(float)(-c)/b;
+    while ( 1 )
+    {
+            if (a == 0 && b == 0)
+                printf("%s", (c == 0) ? "x belong R" : "x belong empty set");
+            else if (a == 0)
+            {                                                                  //IF equation isn't square
+                    x1 = x2 = (-c) / b;
                     printf("This is not square equation, the root is %.3f\n", x1);
             }                                                                  //IF equation is square
-            else{
-                D=b*b-4*a*c;
-                if (D>0){                                                      //if discriminate > 0
-                    D = pow(D,0.5);
-                    x1=(-b-D)/(2*(float)a);
-                    x2=(-b+D)/(2*(float)a);
+            else
+            {
+                D = b * b - 4 * a * c;
+
+                if (D > 0)
+                {                                                               //if discriminate > 0
+                    D = pow(D, 0.5);
+                    x1 = (-b -D) / (2 * a);
+                    x2 = (-b + D)/(2 * a);
+
                     printf ("The roots of equation are %.3f and %.3f\n", x1, x2);
                 }
-                else if (D==0){                                                //if discriminate == 0
-                        x1=x2=((float)-b)/(2*(float)a);
+                else if (D == 0)
+                {                                                               //if discriminate == 0
+                        x1 = x2 =  (-b) / (2 * a);
+
                         printf("The root of equation is %.3f\n", x1);
                 }
-                else                                                           //if discriminate < 0
+                else                                                            //if discriminate < 0
                     printf("The equation haven't roots in R\n");
             }
+
+            while (getchar() != '\n')
+                    continue;
 
             printf("\n\nInput coefficients for equation a*x^2+b*x+c=0 \n");
             printf("Input q if want to continue, else press enter\n");
@@ -52,28 +62,28 @@ int main(void)
                     continue;
 
             printf("a=");
-            a = test_int();
+            a = test_num();
             printf("b=");
-            b = test_int();
+            b = test_num();
             printf("c=");
-            c= test_int();
+            c = test_num();
 
     }
 
     return 0;
 }
 
-int test_int(void)
+double test_num(void)
 {
     char ch = 'A';
-    int x = 0;
+    double x = 0;
 
-    while(scanf("%d", &x) != 1)
+    while(scanf("%lf", &x) != 1)
     {
-        while ( (ch =getchar() ) != '\n')
+        while ( (ch = getchar() ) != '\n')
             putchar(ch);
 
-        printf(" is not int number, please input int number: ");
+        printf(" is not number, please input int number: ");
     }
 
     return x;
