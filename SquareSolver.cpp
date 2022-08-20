951792001fac7d05a7c88092674546a6a9eb5ed1
Demoren1 <mrprofu361@gmail.com>
2022-08-20 16:47:23 +0300

    Добавил указатели

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -2,13 +2,13 @@
 #include <math.h>
 
 double test_num(void);                       //check is input are number, if number - return
-void linnar_equation(double b, double c, double &root1, double &root2, int &flag);
-void square_equation(double a, double b, double c, double &root1, double &root2,int &flag);
-void not_equation (double c, int &flag);
+void linnar_equation(double b, double c, double * root1, int * flag);
+void square_equation(double a, double b, double c, double * root1, double * root2,int * flag);
+void not_equation (double c, int * flag);
 void input_reset(void);
 int  test_exit(void);
-void input (double &a, double &b, double &c);                         //boolean function
-void solver(double a, double b, double c, double &root1, double &root2,int &flag);                             //choose right version of solution
+void input (double * a, double * b, double * c);                         //boolean function
+void solver(double a, double b, double c, double * root1, double * root2,int * flag);                             //choose right version of solution
 void invite_to_input (void);
 void output(double root1, double root2, int flag);
 int main(void)
@@ -21,8 +21,8 @@ int main(void)
             invite_to_input();
             if (test_exit() )
                 break;
-            input(a, b, c);
-            solver (a, b, c, root1, root2, flag);
+            input(&a, &b, &c);
+            solver (a, b, c, &root1, &root2, &flag);
             output(root1, root2, flag);
             input_reset();
     }
@@ -46,43 +46,37 @@ double test_num(void)
     return x;
 }
 
-void not_equation(double c, int &flag)
+void not_equation(double c, int * flag)
 {
-    flag = (c == 0) ? 0 : 1;
-    //printf("%s", (c == 0) ? "x belong R" : "x belong empty set");
+    *flag = (c == 0) ? 0 : 1;
 }
 
-void linnar_equation(double b, double c, double &root1, double &root2, int &flag)
+void linnar_equation(double b, double c, double * root1, int * flag)
 {
-    double x = 0;
-    root1 = root2 = x = (-c) / b;
-    flag = 2;
-    //printf("This is not square equation, the root is %.3f\n", x);
+    *root1 = (-c) / b;
+    *flag = 2;
 }
 
-void square_equation(double a, double b, double c, double &root1, double &root2, int &flag)
+void square_equation(double a, double b, double c, double * root1, double * root2, int * flag)
 {
-    double x1 = 0, x2 = 0, D = 0;
+    double D = 0;
 
     D = b * b - 4 * a * c;
 
     if (D > 0)
     {                                                               //if discriminate > 0
         D = pow(D, 0.5);
-        root1 = x1 = (-b -D) / (2 * a);
-        root2 = x2 = (-b + D)/(2 * a);
-        flag = 3;
-        //printf ("The roots of equation are %.3f and %.3f\n", x1, x2);
+        *root1 = (-b -D) / (2 * a);
+        *root2 = (-b + D)/(2 * a);
+        *flag = 3;
     }
     else if (D == 0)
     {                                                               //if discriminate == 0
-        root1 = root2 = x1 = x2 =  (-b) / (2 * a);
-        flag = 4;
-        //printf("The root of equation is %.3f\n", x1);
+        *root1 = (-b) / (2 * a);
+        *flag = 4;
     }
     else
-        flag = 5;                                                            //if discriminate < 0
-        //printf("The equation haven't roots in R\n");
+        *flag = 5;                                                  //if discriminate < 0
 }
 
 void input_reset(void)
@@ -107,23 +101,22 @@ int test_exit(void)
     return 0;
 }
 
-void input(double &a, double &b, double &c)
+void input(double * a, double * b, double * c)
 {
     printf("a=");
-    a = test_num();
+    *a = test_num();
     printf("b=");
-    b = test_num();
+    *b = test_num();
     printf("c=");
-    c = test_num();
+    *c = test_num();
 }
 
-void solver(double a, double b, double c, double &root1, double &root2, int &flag)
+void solver(double a, double b, double c, double * root1, double * root2, int * flag)
 {
-    double roots[2];
     if (a == 0 && b == 0)
         not_equation(c, flag);
     else if (a == 0)
-        linnar_equation(b, c, root1, root2, flag);
+        linnar_equation(b, c, root1, flag);
     else square_equation(a, b, c, root1, root2, flag);
 }
 
